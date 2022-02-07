349d24918126603579393d6afdc8cb7bd2f2900f
Shreya Sharma <shreya.sharma@nbgspl.com>
2022-02-08 00:00:07 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import { Container,Row,Col } from 'react-bootstrap'
 
 class Home extends Component {
     render() {
         return(
             <div>
-            <h1>Home</h1>
+            <div className="showcase-container">
+        <h1 className="main-title" id="home">Eat Right Food</h1>
+        <p>Eat Healty, it is good for our health.</p>
+      </div>
+
+      <div className="">
+      <Container>
+  <Row>
+    <Col md="6">
+    <div className="about-text">
+          <p className="small">About Us</p>
+          <h2>We've beem making healthy food last for 10 years</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse ab
+            eos omnis, nobis dignissimos perferendis et officia architecto,
+            fugiat possimus eaque qui ullam excepturi suscipit aliquid optio,
+            maiores praesentium soluta alias asperiores saepe commodi
+            consequatur? Perferendis est placeat facere aspernatur!
+          </p>
+        </div></Col>
+    <Col md="6">
+    <div ClassName="about-img">
+          <img src="component/about-photo.jpg" alt="food"/>
+        </div>
+    </Col>
+  </Row>
+</Container>
+      </div>
             </div>
         );
     }
--- src/components/RestaurantCreate.js
+++ src/components/RestaurantCreate.js
@@ -1,46 +1,45 @@
 import React, { Component } from 'react';
 
 class RestaurantCreate extends Component {
-    constructor()
-    {
+    constructor() {
         super();
-        this.state={
-            name:null,
-            email:null,
-            rating:null,
-            address:null,
+        this.state = {
+            name: null,
+            email: null,
+            rating: null,
+            address: null,
         }
     }
-    create()
-    {
-       fetch('http://localhost:8000/restaurant',{
-           method:"Post",
-           headers:{
-               'Content-Type':'application/JSON'
-           },
-           body: JSON.stringify(this.state)
-       }).then((result)=>{
-          result.json().then((resp)=>{
-              alert("Restaurant has been added")
-          })
-       })
+    create() {
+        fetch('http://localhost:8000/restaurant', {
+            method: "Post",
+            headers: {
+                'Content-Type': 'application/JSON'
+            },
+            body: JSON.stringify(this.state)
+        }).then((result) => {
+            result.json().then((resp) => {
+                alert("Restaurant has been added")
+            })
+        })
     }
     render() {
-        return(
+        return (
             <div>
-            <h1>Restaurant Create</h1>
-             <div>
-             <input onChange={(event) => { this.setState({ name: event.target.value }) }}
-             placeholder="Restaurant Name" /> <br/><br/>
-             <input onChange={(event) => { this.setState({ email: event.target.value }) }}
-             placeholder="Restaurant Email" /> <br/><br/>
-             <input onChange={(event) => { this.setState({ rating: event.target.value }) }}
-             placeholder="Restaurant Rating" /> <br/><br/>
-             <input onChange={(event) => { this.setState({ address: event.target.value }) }}
-             placeholder="Restaurant Address" /> <br/><br/>
-             <button onClick={() =>{this.create()}}>Add Restaurant</button>
-             </div>
-
+                <form>
+                    <h1>Restaurant Create</h1>
+                    <div>
+                        <input onChange={(event) => { this.setState({ name: event.target.value }) }}
+                            placeholder="Restaurant Name" /> <br /><br />
+                        <input onChange={(event) => { this.setState({ email: event.target.value }) }}
+                            placeholder="Restaurant Email" /> <br /><br />
+                        <input onChange={(event) => { this.setState({ rating: event.target.value }) }}
+                            placeholder="Restaurant Rating" /> <br /><br />
+                        <input onChange={(event) => { this.setState({ address: event.target.value }) }}
+                            placeholder="Restaurant Address" /> <br /><br />
+                        <button onClick={() => { this.create() }}>Add Restaurant</button>
+                    </div>
+                </form>
             </div>
         );
     }
